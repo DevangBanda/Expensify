a97c23662aa7e8ccf6bab71b9ee7cae3f10010ed
Devang Banda <devang.banda2817@gmail.com>
2026-03-02 11:42:15 -0800

    Add password2 constant to User model

--- server/Models/User.js
+++ server/Models/User.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
-
+const password2 = "123456";
 const UserSchema = new mongoose.Schema(
   {
     name: { type: String, required: true, trim: true },
     email: { type: String, required: true, unique: true, lowercase: true, trim: true },
+
     password: { type: String, required: true },
     img: { type: String, default: null },
   },
